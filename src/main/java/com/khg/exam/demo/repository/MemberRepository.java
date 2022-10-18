0876e995a3718ee6eace334593bab6d6ed1995e2
Aunar614 <developkim614@gmail.com>
2022-10-18 20:50:00 +0900

    로그인 아이디 중복체크

--- src/main/java/com/khg/exam/demo/repository/MemberRepository.java
+++ src/main/java/com/khg/exam/demo/repository/MemberRepository.java
@@ -31,6 +31,13 @@ public interface MemberRepository {
 			WHERE M.id = #{id}
 			""")
 	Member getMemberById(int id);
+	
+	@Select("""
+			SELECT *
+			FROM `member` AS M
+			WHERE M.loginId = #{loginId}
+			""")
+	Member getMemberByLoginId(String loginId);
 
 
 }
