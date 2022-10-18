fb8125de82978560c756e1187e889d63c1a4d17c
Aunar614 <developkim614@gmail.com>
2022-10-18 18:44:09 +0900

    회원가입 준비, UsrMemberController, MemberService 추가

---
+++ src/main/java/com/khg/exam/demo/controller/UsrMemberController.java
@@ -0,0 +1,26 @@
+package com.khg.exam.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.khg.exam.demo.servicer.MemberService;
+
+@Controller
+public class UsrMemberController {
+	
+	@Autowired
+	private MemberService memberService;
+	
+	@RequestMapping("usr/member/doJoin")
+	@ResponseBody
+	public String doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
+		
+		memberService.join(loginId, loginPw, name, nickname, cellphoneNum, email);
+		
+		return "가입!";
+	}
+	
+}
+
---
+++ src/main/java/com/khg/exam/demo/servicer/MemberService.java
@@ -0,0 +1,14 @@
+package com.khg.exam.demo.servicer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+//	private MemberRepository memberRepository;
+		
+	public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
+			String email) {
+//		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNum, email);
+	}
+
+}
