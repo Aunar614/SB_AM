b5e77d9e195590cfd3e3e6b5966cb08609b4f3ef
Aunar614 <developkim614@gmail.com>
2022-10-21 17:45:50 +0900

    로그인 기능 구현(session)

--- src/main/java/com/khg/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/khg/exam/demo/controller/UsrArticleController.java
@@ -14,12 +14,11 @@ import com.khg.exam.demo.vo.ResultData;
 
 @Controller
 public class UsrArticleController {
-	
+
 	// 인스턴스 변수
 	@Autowired
 	private ArticleService articleService;
-	
-	
+
 	// 액션메서드
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
@@ -30,61 +29,61 @@ public class UsrArticleController {
 		if (Ut.empty(body)) {
 			return ResultData.from("F-2", "내용을 입력해주세요");
 		}
-		
+
 		ResultData<Integer> writeArticleRd = articleService.writeArticle(title, body);
 		int id = (int) writeArticleRd.getData1();
 		Article article = articleService.getArticle(id);
-		
+
 		return ResultData.newData(writeArticleRd, article);
 	}
-	
+
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
 	public ResultData<List<Article>> getArticles() {
 		List<Article> articles = articleService.getArticles();
-		
+
 		return ResultData.from("S-1", "Article List", articles);
 	}
-	
+
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
 	public ResultData<Integer> doDelete(int id) {
 		Article article = articleService.getArticle(id);
-		
+
 		if (article == null) {
-			
+
 			return ResultData.from("F-1", Ut.f("%d번 게시물은 존재하지 않습니다", id), id);
 		}
-		
+
 		articleService.deleteArticle(id);
-		
+
 		return ResultData.from("S-1", Ut.f("%d번 게시물을 삭제했습니다", id), id);
 	}
-	
+
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
 	public ResultData<Integer> doModify(int id, String title, String body) {
 		Article article = articleService.getArticle(id);
-		
+
 		if (article == null) {
 			return ResultData.from("F-1", Ut.f("%d번 게시물은 존재하지 않습니다", id), id);
 		}
-		
+
 		articleService.modifyArticle(id, title, body);
-		
+
 		return ResultData.from("F-1", Ut.f("%d번 게시물을 수정했습니다", id), id);
 	}
-	
+
 	@RequestMapping("/usr/article/getArticle")
 	@ResponseBody
 	public ResultData<Article> getArticle(int id) {
 		Article article = articleService.getArticle(id);
-		
+
 		if (article == null) {
 
 			return ResultData.from("f-1", Ut.f("%d번 게시물은 존재하지 않습니다", id));
 		}
-		
+
 		return ResultData.from("S-1", Ut.f("%d번 게시물입니다", id), article);
 	}
 
--- src/main/java/com/khg/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/khg/exam/demo/controller/UsrMemberController.java
@@ -1,5 +1,7 @@
 package com.khg.exam.demo.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,52 +14,93 @@ import com.khg.exam.demo.vo.ResultData;
 
 @Controller
 public class UsrMemberController {
-	
+
 	@Autowired
 	private MemberService memberService;
-	
+
 	@RequestMapping("usr/member/doJoin")
 	@ResponseBody
-	public ResultData<Member> doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
-		
+	public ResultData<Member> doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
+			String email) {
+
 		if (Ut.empty(loginId)) {
 			return ResultData.from("F-1", "아이디를 입력해주세요");
 		}
-		
+
 		if (Ut.empty(loginPw)) {
 			return ResultData.from("F-2", "비밀번호를 입력해주세요");
 		}
-		
+
 		if (Ut.empty(name)) {
 			return ResultData.from("F-3", "이름을 입력해주세요");
 		}
-		
+
 		if (Ut.empty(nickname)) {
 			return ResultData.from("F-4", "닉네임을 입력해주세요");
 		}
-		
+
 		if (Ut.empty(cellphoneNum)) {
 			return ResultData.from("F-5", "전화번호를 입력해주세요");
 		}
-		
+
 		if (Ut.empty(email)) {
 			return ResultData.from("F-6", "이메일을 입력해주세요");
 		}
-		
+
 		// S-1
 		// 회원가입이 완료 되었습니다
 		// F-1~8
 		// 실패
 		ResultData<Integer> joinRd = memberService.join(loginId, loginPw, name, nickname, cellphoneNum, email);
-		
+
 		if (joinRd.isFail()) {
 			return (ResultData) joinRd;
 		}
-		
+
 		Member member = memberService.getMemberById(joinRd.getData1());
-		
+
 		return ResultData.newData(joinRd, member);
 	}
-	
-}
 
+	@RequestMapping("usr/member/doLogin")
+	@ResponseBody
+	public ResultData<Member> doLogin(HttpSession httpsession, String loginId, String loginPw) {
+		boolean isLogined = false;
+
+		if (httpsession.getAttribute("loginedMemberId") != null) {
+			isLogined = true;
+		}
+
+		if (isLogined) {
+			return ResultData.from("F-5", "이미 로그인 되었습니다");
+		}
+
+		if (Ut.empty(loginId)) {
+			return ResultData.from("F-1", "아이디를 입력해주세요");
+		}
+
+		if (Ut.empty(loginPw)) {
+			return ResultData.from("F-2", "비밀번호를 입력해주세요");
+		}
+
+		// S-1
+		// 회원가입이 완료 되었습니다
+		// F-1~8
+		// 실패
+
+		Member member = memberService.getMemberByLoginId(loginId);
+
+		if (member == null) {
+			return ResultData.from("F-3", "아이디를 잘못 입력했습니다");
+		}
+
+		if (member.getLoginPw().equals(loginPw) == false) {
+			return ResultData.from("F-4", "비밀번호가 일치하지 않습니다");
+		}
+
+		httpsession.setAttribute("loginedMemberId", member.getId());
+
+		return ResultData.from("S-1", Ut.f("%s님 환영합니다", member.getNickname()));
+	}
+
+}
