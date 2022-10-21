8ac8fa5422192d2d9f0bad6d66c6d5f4d31e9f3a
Aunar614 <developkim614@gmail.com>
2022-10-21 19:14:23 +0900

    ResultData에 data1Name 추가

--- src/main/java/com/khg/exam/demo/service/ArticleService.java
+++ src/main/java/com/khg/exam/demo/service/ArticleService.java
@@ -38,7 +38,7 @@ public class ArticleService {
 		articleRepository.writeArticle(memberId, title, body);
 		int id = articleRepository.getLastInsertId();
 		
-		return ResultData.from("S-1", Ut.f("%d번 게시물이 생성되었습니다", id), id);
+		return ResultData.from("S-1", Ut.f("%d번 게시물이 생성되었습니다", id), "id", id);
 	}
 
 	public void deleteArticle(int id) {
@@ -52,7 +52,7 @@ public class ArticleService {
 		
 		Article article = getArticle(id);
 		
-		return ResultData.from("S-1", Ut.f("%d번 게시물을 수정했습니다", id), article);
+		return ResultData.from("S-1", Ut.f("%d번 게시물을 수정했습니다", id), "article", article);
 	}
 
 	public ResultData actorCanModfy(int loginedMemberId, Article article) {
--- src/main/java/com/khg/exam/demo/vo/ResultData.java
+++ src/main/java/com/khg/exam/demo/vo/ResultData.java
@@ -11,6 +11,8 @@ public class ResultData<DT> {
 	private String msg;
 	@Getter
 	private DT data1;
+	@Getter
+	private String data1Name;
 
 	private ResultData() {
 
@@ -18,13 +20,14 @@ public class ResultData<DT> {
 
 	public static <DT> ResultData<DT> from(String resultCode, String msg) {
 
-		return from(resultCode, msg, null);
+		return from(resultCode, msg, null, null);
 	}
 
-	public static <DT> ResultData<DT> from(String resultCode, String msg, DT data1) {
+	public static <DT> ResultData<DT> from(String resultCode, String msg, String data1Name, DT data1) {
 		ResultData<DT> rd = new ResultData<DT>();
 		rd.resultCode = resultCode;
 		rd.msg = msg;
+		rd.data1Name = data1Name;
 		rd.data1 = data1;
 
 		return rd;
@@ -38,9 +41,9 @@ public class ResultData<DT> {
 		return isSuccess() == false;
 	}
 
-	public static <DT> ResultData<DT> newData(ResultData joinRd, DT newData) {
+	public static <DT> ResultData<DT> newData(ResultData Rd, String data1Name, DT data1) {
 		
-		return from(joinRd.getResultCode(), joinRd.getMsg(), newData);
+		return from(Rd.getResultCode(), Rd.getMsg(), data1Name, data1);
 	}
 
 }
