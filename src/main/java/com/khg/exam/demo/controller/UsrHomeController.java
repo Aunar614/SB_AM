a9a66758910113ff9d12bbee57ef5aae0a33efc5
Aunar614 <developkim614@gmail.com>
2022-10-24 16:35:34 +0900

    JSP 연결

--- src/main/java/com/khg/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/khg/exam/demo/controller/UsrHomeController.java
@@ -7,11 +7,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class UsrHomeController {
 	
-	@RequestMapping("usr/home/main")
-	@ResponseBody
-	public String getString() {
+	@RequestMapping("/usr/home/main")
+	public String showMain() {
 		
-		return "hi";
+		return "usr/home/main";
+	}
+	
+	@RequestMapping("/")
+	public String showRoot() {
+		
+		return "redirect:/usr/home/main";
 	}
 }
 
