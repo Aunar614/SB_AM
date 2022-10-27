9400773bb7848c19dfcf0862e8f65147fc4ed072
Aunar614 <developkim614@gmail.com>
2022-10-27 18:26:12 +0900

    modify form을 보여주기 전에 체크, modify form 제작, url로 수정하려는 시도 방어, 수정 후에 replace

--- src/main/java/com/khg/exam/demo/vo/Rq.java
+++ src/main/java/com/khg/exam/demo/vo/Rq.java
@@ -41,14 +41,7 @@ public class Rq {
 	public void printHistoryBackJs(String msg) throws IOException {
 		resp.setContentType("text/html; charset=UTF-8");
 
-		println("<script>");
-
-		if (!Ut.empty(msg)) {
-			println("alert('" + msg + "');");
-		}
-
-		println("history.back()");
-		println("</script>");
+		print(Ut.jsHistoryBack(msg));
 
 	}
 
@@ -71,6 +64,13 @@ public class Rq {
 	public void logout() {
 		session.removeAttribute("loginedMemberId");
 
+	}
+
+	public String jsHistoryBackOnView(String msg) {
+		req.setAttribute("msg", msg);
+		req.setAttribute("historyBack", true);
+		
+		return "usr/common/js";
 	}
 
 }
