d5c044a693aeeb09ed01b90b9f4ee2ca50781863
Aunar614 <developkim614@gmail.com>
2022-11-02 18:29:30 +0900

    article list 페이징 (파라미터 처리)

--- src/main/java/com/khg/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/khg/exam/demo/controller/UsrArticleController.java
@@ -2,12 +2,11 @@ package com.khg.exam.demo.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.khg.exam.demo.service.ArticleService;
@@ -32,18 +31,18 @@ public class UsrArticleController {
 	// 액션메서드
 	@RequestMapping("/usr/article/write")
 	public String showWrite(String title, String body) {
-		
+
 		return "usr/article/write";
 	}
-	
+
 	@RequestMapping("/usr/article/doWrite")
 	@ResponseBody
 	public String doWrite(int boardId, String title, String body, String replaceUri) {
-		
+
 		if (Ut.empty(title)) {
 			return rq.jsHistoryBack("제목을 입력해주세요");
 		}
-		
+
 		if (Ut.empty(body)) {
 			return rq.jsHistoryBack("내용을 입력해주세요");
 		}
@@ -55,27 +54,31 @@ public class UsrArticleController {
 		if (Ut.empty(replaceUri)) {
 			replaceUri = Ut.f("../article/detail?id=%d", id);
 		}
-		
+
 		return rq.jsReplace(Ut.f("%d번 글이 생성되었습니다", id), replaceUri);
 	}
-	
+
 	@RequestMapping("/usr/article/list")
-	public String showList(Model model, int boardId) {
-		
+	public String showList(Model model, @RequestParam(defaultValue = "1") int boardId,
+			@RequestParam(defaultValue = "1") int page) {
+
 		Board board = boardService.getBoardById(boardId);
-		
+
 		if (board == null) {
 			return rq.jsHistoryBackOnView("존재하지 않는 게시판입니다");
 		}
-		
+
 		int articlesCount = articleService.getArticlesCount(boardId);
-		
-		List<Article> articles = articleService.getForPrintArticles(rq.getLoginedMemberId(), boardId);
+
+		int itemsInAPage = 10;
+
+		List<Article> articles = articleService.getForPrintArticles(rq.getLoginedMemberId(), boardId, page,
+				itemsInAPage);
 
 		model.addAttribute("board", board);
 		model.addAttribute("articlesCount", articlesCount);
 		model.addAttribute("articles", articles);
-		
+
 		return "usr/article/list";
 	}
 
@@ -89,7 +92,7 @@ public class UsrArticleController {
 
 			return rq.jsHistoryBack(Ut.f("%d번 게시물은 존재하지 않습니다", id));
 		}
-		
+
 		if (article.getMemberId() != rq.getLoginedMemberId()) {
 			return rq.jsHistoryBack(Ut.f("%d번 게시물에 대한 권한이 없습니다", id));
 		}
@@ -98,54 +101,53 @@ public class UsrArticleController {
 
 		return rq.jsReplace(Ut.f("%d번 게시물을 삭제했습니다", id), "../article/list?boardId=1");
 	}
-	
+
 	@RequestMapping("/usr/article/modify")
 	public String showModify(int id, Model model) {
-		
+
 		Article article = articleService.getForPrintArticle(rq.getLoginedMemberId(), id);
 
 		if (article == null) {
 			return rq.jsHistoryBackOnView(Ut.f("%d번 게시물은 존재하지 않습니다", id));
 		}
-		
+
 		ResultData actorCanModifyRd = articleService.actorCanModify(rq.getLoginedMemberId(), article);
-		
+
 		if (actorCanModifyRd.isFail()) {
 			return rq.jsHistoryBackOnView(actorCanModifyRd.getMsg());
 		}
-		
+
 		model.addAttribute("article", article);
-		
+
 		return "usr/article/modify";
 	}
 
-	
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
 	public String doModify(int id, String title, String body) {
-		
+
 		Article article = articleService.getForPrintArticle(rq.getLoginedMemberId(), id);
 
 		if (article == null) {
 			return rq.jsHistoryBack(Ut.f("%d번 게시물은 존재하지 않습니다", id));
 		}
-		
+
 		ResultData actorCanModifyRd = articleService.actorCanModify(rq.getLoginedMemberId(), article);
-		
+
 		if (actorCanModifyRd.isFail()) {
 			return rq.jsHistoryBack(actorCanModifyRd.getMsg());
 		}
-		
+
 		articleService.modifyArticle(id, title, body);
-		
+
 		return rq.jsReplace(Ut.f("%d번 게시물을 수정했습니다", id), Ut.f("../article/detail?id=%d", id));
 	}
 
 	@RequestMapping("/usr/article/detail")
 	public String showDetail(Model model, int id) {
-		
+
 		Article article = articleService.getForPrintArticle(rq.getLoginedMemberId(), id);
-		
+
 		model.addAttribute("article", article);
 
 		return "usr/article/detail";
