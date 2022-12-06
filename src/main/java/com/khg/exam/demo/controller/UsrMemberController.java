96f651b49c453318310fd1e73d68b6c29f63e8d0
Aunar614 <developkim614@gmail.com>
2022-12-06 19:16:42 +0900

    로그인 아이디 찾기

--- src/main/java/com/khg/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/khg/exam/demo/controller/UsrMemberController.java
@@ -35,7 +35,7 @@ public class UsrMemberController {
 
 		return rq.jsReplace("회원가입이 완료되었습니다. 로그인 후 이용해주세요", afterJoinUri);
 	}
-	
+
 	@RequestMapping("usr/member/getLoginIdDup")
 	@ResponseBody
 	public ResultData getLoginIdDup(String loginId) {
@@ -89,6 +89,25 @@ public class UsrMemberController {
 		rq.login(member);
 
 		return Ut.jsReplace(Ut.f("%s님 환영합니다", member.getNickname()), afterLoginUri);
+	}
+
+	@RequestMapping("usr/member/findLoginId")
+	public String showFindLoginId() {
+		return "usr/member/findLoginId";
+	}
+
+	@RequestMapping("usr/member/doFindLoginId")
+	@ResponseBody
+	public String doFindLoginId(String name, String email,
+			@RequestParam(defaultValue = "/") String afterFindLoginIdUri) {
+
+		Member member = memberService.getMemberByNameAndEmail(name, email);
+
+		if (member == null) {
+			return Ut.jsHistoryBack("존재하지 않는 이름 또는 이메일입니다");
+		}
+
+		return Ut.jsReplace(Ut.f("회원님의 아이디는 [ %s ] 입니다", member.getLoginId()), afterFindLoginIdUri);
 	}
 
 	@RequestMapping("usr/member/doLogout")
